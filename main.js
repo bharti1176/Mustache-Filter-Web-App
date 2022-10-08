480d9afe2d4d977b4d493b69e7c7cb98c52df97e
bharti1176 <97240673+bharti1176@users.noreply.github.com>
2022-10-08 13:52:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+ canvas=createCanvas(300,300);
+ canvas.center();
+
+ video=createCapture(VIDEO);
+ video.size(300,300);
+ video.hide();
+
+ poseNet=ml5.poseNet(video,modelloaded);
+ poseNet.on("pose",gotPoses);
+
+}
+function modelloaded()
+{
+  console.log("model is inital initialized");
+  
+}
+function gotPoses(results)
+{
+  if (results.length>0)
+  {
+    console.log(results);
+    console.log("mustache x="+results[0].pose.nose.x);
+    console.log("mustache y="+results[0].pose.nose.y);
+  }
+}
+function draw()
+{
+image(video,0,0,300,300);
+}
+
+function take_snapshot()
+{
+  save("ByAdvay.png")
+}
+
